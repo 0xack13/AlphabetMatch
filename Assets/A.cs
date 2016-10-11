d45714b11e7f970028e1bba63e1380d591e1b718
0xack13 <0xack13@gmail.com>
2016-10-11 14:00:14 +0300

    initial commit

---
+++ Assets/A.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class A : MonoBehaviour {
+
+	private static GameManager gameManager;
+
+	// Use this for initialization
+	void Start () {
+		if (gameManager == null) {
+			gameManager = GameObject.FindObjectOfType<GameManager> ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnAMouseClick(A a) {
+		gameManager.OnAMouseClick (a);
+	}
+}
